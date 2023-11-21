3891d34a55940818e85694a7ea11f87418f6b753
rohrig <richard.t.rohrig@gmail.com>
2023-11-21 15:46:34 +0100

    chore: add sfui

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex-col align-middle">
+    <Carousel />
+    <NewsLetterForm />
+    <div class=" flex justify-center">
+      <Banner />
+    </div>
+  </div>
+</template>
